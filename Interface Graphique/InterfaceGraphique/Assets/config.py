fb53b46af7f13c6ed4cd22c37bccd4725340e141
kh-salma <khallad.salma20@gmail.com>
2025-01-17 15:16:38 +0100

    Add converted image file paths and types to config.py

--- Interface Graphique/InterfaceGraphique/Assets/config.py
+++ Interface Graphique/InterfaceGraphique/Assets/config.py
@@ -1,3 +1,20 @@
 gray_image_type = ["gray_uniform", "gray_601", "gray_907"]
 h_image_type = ["hsv", "hsl"]
 indexed_image_type = ["indexed_222", "indexed_444", "indexed_888"]
+converted_base_path = 'C:\\Users\\salma\\OneDrive\\Documents\\URCA - M2\\INF00903\\Projet\\Interface Graphique\\InterfaceGraphique\\Assets\\Json Files\\converted_BD_images'
+converted_json_files = [
+    'cmyk__converted_images.json',
+    'grey_601__converted_images.json',
+    'grey_907__converted_images.json',
+    'grey_uniform__converted_images.json',
+    'hsl__converted_images.json',
+    'hsv__converted_images.json',
+    'i1i2i3__converted_images.json',
+    'indexed_222__converted_images.json',
+    'indexed_444__converted_images.json',
+    'indexed_888__converted_images.json',
+    'nrgb__converted_images.json',
+    'rgb__converted_images.json',
+    'yiq__converted_images.json',
+    'yuv__converted_images.json'
+]
